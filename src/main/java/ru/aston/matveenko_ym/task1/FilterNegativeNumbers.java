249309b9448c4fbc8848e28ed4f1d26ec02d234e
KoRn <korn1984@narod.ru>
2025-02-27 21:50:03 +0300

    task1 (Stream API examples).

---
+++ src/main/java/ru/aston/matveenko_ym/task1/FilterNegativeNumbers.java
@@ -0,0 +1,17 @@
+package ru.aston.matveenko_ym.task1;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Отфильтруйте все элементы списка, которые меньше 0.
+ */
+public class FilterNegativeNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(-5, 3, -1, 7, 0, -2, 4, 8);
+        List<Integer> filteredNumbers = numbers.stream()
+                .filter(n -> n >= 0)
+                .toList();
+        System.out.println("Отфильтрованный список: " + filteredNumbers);
+    }
+}
