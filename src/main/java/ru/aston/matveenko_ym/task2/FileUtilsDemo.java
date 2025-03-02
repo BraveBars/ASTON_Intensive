ca2865892e62ac2628556ca82898c234e80c1c83
KoRn <korn1984@narod.ru>
2025-03-02 17:30:28 +0300

    task2 (development finished).

--- src/main/java/ru/aston/matveenko_ym/task2/FileUtilsDemo.java
+++ src/main/java/ru/aston/matveenko_ym/task2/FileUtilsDemo.java
@@ -9,24 +9,28 @@ import java.util.List;
 public class FileUtilsDemo {
     public static void main(String[] args) {
         final Logger log = LoggerFactory.getLogger(FileUtilsDemo.class);
+        final String FILE_PATH_INPUT = ".\\src\\main\\resources\\files\\input.txt";
+        final String FILE_PATH_OUTPUT = ".\\src\\main\\resources\\files\\output.txt";
+        final String FILE1_PATH = ".\\src\\main\\resources\\files\\file1.txt";
+        final String FILE2_PATH = ".\\src\\main\\resources\\files\\file2.txt";
+        final String FILE_PATH_MERGED = ".\\src\\main\\resources\\files\\merged.txt";
 
         try {
             // Чтение файла.
-            List<String> lines = FileUtils.readFile(".\\src\\main\\resources\\input.txt");
+            List<String> lines = FileUtils.readFile(FILE_PATH_INPUT);
             System.out.println("Содержимое файла 'input.txt': \n");
             lines.forEach(System.out::println);
             System.out.println("---------------------------------------------------------------");
 
             // Запись строки в файл.
-            FileUtils.writeFile(".\\src\\main\\resources\\output.txt", "Привет, мир!\n");
+            FileUtils.writeFile(FILE_PATH_OUTPUT, "Привет, мир!\n");
 
             // Склеивание двух файлов.
-            FileUtils.mergeFiles(".\\src\\main\\resources\\file1.txt",
-                    ".\\src\\main\\resources\\file2.txt", ".\\src\\main\\resources\\merged.txt");
+            FileUtils.mergeFiles(FILE1_PATH, FILE2_PATH, FILE_PATH_MERGED);
 
             // Замена символов в файле.
-            FileUtils.replaceNonAlphanumeric(".\\src\\main\\resources\\input.txt");
-            System.out.println("Файл 'input.txt' обработан (замена символов).");
+            FileUtils.replaceNonAlphanumeric(FILE_PATH_INPUT);
+            System.out.println("Файл 'input.txt' обработан (замена символов)!");
 
         } catch (IOException ioException) {
             log.error("Error reading from file or writing to file!", ioException);
